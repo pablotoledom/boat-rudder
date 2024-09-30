a0c1b717ca5dfb849707917e54a15546fa88b643
Jonathan Toledo <pablotoledomoya@gmail.com>
2024-09-30 11:54:56 -0300

    I added curl library to avoid the operating system having to search for it

--- src/api/google_sheets_api.c
+++ src/api/google_sheets_api.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <curl/curl.h>
+#include "../utils/curl/curl.h"
 #include "google_sheets_api.h"
 
 // Struct to hold response data from Google Sheets API
